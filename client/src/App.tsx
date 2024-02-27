49342f7fd07c858631e3aaccba27cf51ed41b687
Bryan <bryandang10@gmail.com>
2024-02-26 17:49:23 -0800

    create form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,34 +3,62 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import '@mantine/core/styles.css';
-
 import { MantineProvider } from '@mantine/core';
 
+import { useForm } from '@mantine/form';
+import { Text, Title, TextInput, Button, Group, Box, Divider } from '@mantine/core';
+import { randomId } from '@mantine/hooks';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const form = useForm({
+    initialValues: {
+      name: '',
+      ingredients: '',
+      directions: '',
+    },
+  });
 
   return (
     <MantineProvider>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Box mt="3rem" maw={600} mx="auto">
+        <Title order={1}>Recipe App</Title>
+        
+        <Box mt="2rem" maw={500} mx="auto">
+          <Title order={3}>New Recipe</Title>
+
+          <TextInput mt="md" label="Recipe Name" placeholder="Recipe Name" {...form.getInputProps('name')} />
+          <TextInput mt="md" label="Ingredients" placeholder="Ingredients" {...form.getInputProps('ingredients')} />
+          <TextInput mt="md" label="Directions" placeholder="Directions" {...form.getInputProps('directions')} />
+
+          <Group justify="left" mt="md">
+            <Button onClick={() => form.reset()}>
+              Clear
+            </Button>
+            <Button
+              onClick={() =>
+                form.setValues({
+                  name: randomId(),
+                  ingredients: randomId(),
+                  directions: randomId(),
+                })
+              }
+            >
+              Submit
+            </Button>
+          </Group>
+        </Box>
+
+        <Divider my="md"></Divider>
+
+        <Box mt="2rem" maw={500} mx="auto">
+          <Title order={3}>Saved Recipes</Title>
+          <Group justify="left" mt="md">
+            <Text>Looks like there are no recipes!</Text>
+          </Group>
+        </Box>
+      </Box>
+
     </MantineProvider>
   )
 }
