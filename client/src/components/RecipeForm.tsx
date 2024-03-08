187dcaea70659a0d0f346bae63297c671eeec461
Bryan <bryandang10@gmail.com>
2024-03-08 01:30:02 -0800

    refactor components

--- client/src/components/RecipeForm.tsx
+++ client/src/components/RecipeForm.tsx
@@ -9,31 +9,29 @@ export default function RecipeForm() {
       directions: '',
     },
     validate: {
-      name: isNotEmpty('Recipe is required'),
+      name: isNotEmpty('Recipe name is required'),
       ingredients: isNotEmpty("Ingredients are required"),
       directions: isNotEmpty("Directions are required"),
     },
   });
 
   return (
-    <div>
-      <Box m="1rem">
-        <Title order={3}>Add a New Recipe</Title>
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
-          <TextInput mt="md" withAsterisk label="Recipe Name" placeholder="Recipe Name" {...form.getInputProps('name')} />
-          <Textarea mt="md" withAsterisk label="Ingredients" placeholder="Ingredients" {...form.getInputProps('ingredients')} />
-          <Textarea mt="md" withAsterisk label="Directions" placeholder="Directions" {...form.getInputProps('directions')} />
+    <Box m="1rem">
+      <Title order={3}>Add a New Recipe</Title>
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput mt="md" withAsterisk label="Recipe Name" placeholder="Recipe Name" {...form.getInputProps('name')} />
+        <Textarea mt="md" withAsterisk label="Ingredients" placeholder="Ingredients" {...form.getInputProps('ingredients')} />
+        <Textarea mt="md" withAsterisk label="Directions" placeholder="Directions" {...form.getInputProps('directions')} />
 
-          <Group justify="left" mt="md">
-            <Button color="gray" onClick={() => form.reset()}>
-              Reset
-            </Button>
-            <Button type="submit">
-              Save
-            </Button>
-          </Group>
-        </form>
-      </Box>
-    </div>
+        <Group justify="left" mt="md">
+          <Button color="gray" onClick={() => form.reset()}>
+            Reset
+          </Button>
+          <Button type="submit">
+            Save
+          </Button>
+        </Group>
+      </form>
+    </Box>
   )
 }
--- client/src/components/Recipes.tsx
+++ client/src/components/Recipes.tsx
@@ -1,33 +1,90 @@
+import { Text, Title, Anchor, Modal, Box, Divider, Accordion, ActionIcon, AccordionControlProps, Center, Tooltip } from '@mantine/core';
+import { IconEdit, IconTrash } from '@tabler/icons-react';
 import { useDisclosure } from '@mantine/hooks';
-import { Text, Title, Anchor, Modal, Box, Divider } from '@mantine/core';
+
+function AccordionControl(props: AccordionControlProps) {
+  return (
+    <Center>
+      <Accordion.Control {...props} />
+      <Tooltip label="Edit">
+        <ActionIcon onClick={EditRecipe} size="lg" variant="subtle" color="gray" mr="5">
+          <IconEdit size="1rem" />
+        </ActionIcon>
+      </Tooltip>
+      <Tooltip label="Delete">
+        <ActionIcon onClick={DeleteRecipe} size="lg" variant="subtle" color="gray" mr="10">
+          <IconTrash size="1rem" />
+        </ActionIcon>
+      </Tooltip>
+    </Center>
+  );
+}
+
+function EditRecipe() {
+
+
+  alert("edit");
+  // open a modal and fill it with the details from the recipe that was clicked
+}
+
+function DeleteRecipe() {
+  alert("delete");
+  // make the api call to delete
+  // refresh the recipes list
+}
 
 export default function Recipes() {
-  const [opened, { open, close }] = useDisclosure(false);
+  const dummyItems = [
+    { value: 'Item 1', emoji: '🍎', description: 'Description for Item 1' },
+    { value: 'Item 2', emoji: '🍌', description: 'Description for Item 2' },
+    { value: 'Item 3', emoji: '🍇', description: 'Description for Item 3' },
+  ];
+
+  // Map through the dummy data to generate Accordion items
+  const items = dummyItems.map((item) => (
+    <Accordion.Item key={item.value} value={item.value}>
+      <Accordion.Control icon={item.emoji}>{item.value}</Accordion.Control>
+      <Accordion.Panel>{item.description}</Accordion.Panel>
+    </Accordion.Item>
+  ));
 
   return (
     <div>
       <Box m="1rem">
-        <Title order={3}>Saved Recipes</Title>
+        <Title order={3} pb="15">Saved Recipes</Title>
 
-        <Modal opened={opened} onClose={close} title="Recipe Name" centered>
+        {/* <Accordion variant="separated" defaultValue={"Test"}> */}
+        {/* {items} */}
+        {/* </Accordion> */}
 
-          <Text color='gray'>Date: January 8 2004</Text>
+        <Accordion variant="separated" chevronPosition="left">
+          <Accordion.Item value="item-1">
+            <AccordionControl>Recipe 1</AccordionControl>
+            <Accordion.Panel>
+              <Text color='gray' pb="10">Date: 4/15/2023</Text>
 
-          <Divider my="md"></Divider>
+              <Box pb="10">
+                <Title order={5}>Ingredients</Title>
+                <Text>Ingredients...</Text>
+              </Box>
 
-          <Title order={5}>Ingredients</Title>
-          <Text>Ingredients go here...</Text>
+              <Box pb="10">
+                <Title order={5}>Directions</Title>
+                <Text>Directions...</Text>
+              </Box>
+            </Accordion.Panel>
+          </Accordion.Item>
 
-          <Divider my="md"></Divider>
+          <Accordion.Item value="item-2">
+            <AccordionControl>Recipe 2</AccordionControl>
+            <Accordion.Panel>Description 2</Accordion.Panel>
+          </Accordion.Item>
 
-          <Title order={5}>Directions</Title>
-          <Text>Directions go here...</Text>
-
-        </Modal>
-
-        {/* This should be auto generated from localStorage (or from the PostgreSQL db) */}
-        <Text mt="xs"><Anchor onClick={open}>Recipe One</Anchor></Text>
-        <Text mt="xs"><Anchor onClick={open}>Recipe Two</Anchor></Text>
+          <Accordion.Item value="item-3">
+            <AccordionControl>Recipe 3</AccordionControl>
+            <Accordion.Panel>Description 3</Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
       </Box>
     </div>
   )
