04d1ec011e83d7fd6883670e4b5a295e3af14263
Bryan <bryandang10@gmail.com>
2024-03-09 13:22:46 -0800

    add uuid and form addition

--- client/src/components/RecipeForm.tsx
+++ client/src/components/RecipeForm.tsx
@@ -1,5 +1,7 @@
 import { isNotEmpty, useForm } from '@mantine/form';
 import { Title, Textarea, TextInput, Button, Group, Box } from '@mantine/core';
+import { v4 as uuidv4 } from 'uuid'
+import axios from "axios";
 
 interface FormData {
   name: string;
@@ -8,14 +10,21 @@ interface FormData {
 }
 
 function handleOnSubmit(formData: FormData) {
-  alert(new Date().toLocaleString())
-  console.log(formData)
+  const recipeData = {
+    id: uuidv4(),
+    timeLastModified: new Date().toLocaleString(),
+    recipeName: formData.name,
+    recipeIngredients: formData.ingredients,
+    recipeDirections: formData.directions
+  }
 
-  // const created = new Date().toLocaleString();
-  // const id = `${currentDate}-${Math.floor(Math.random() * 100000)}`;
-  // formData.name
-  // formData.ingredients
-  // formData.directions
+  // TODO: Change the actual API endpoint to a correct one (handle the comma separation on the backend)
+  axios.post("https://localhost:8080/add", recipeData).then((response) => {
+    console.log(response.status, response.data.token)
+  })
+  .catch((error) => {
+    console.log("Error:", error)
+  })
 }
 
 export default function RecipeForm() {
@@ -36,9 +45,9 @@ export default function RecipeForm() {
     <Box m="1rem">
       <Title order={3}>Add a New Recipe</Title>
       <form onSubmit={form.onSubmit(() => handleOnSubmit(form.values))}>
-        <TextInput mt="md" withAsterisk label="Recipe Name" placeholder="Recipe Name" {...form.getInputProps('name')} />
-        <Textarea mt="md" withAsterisk label="Ingredients" placeholder="Ingredients" {...form.getInputProps('ingredients')} />
-        <Textarea mt="md" withAsterisk label="Directions" placeholder="Directions" {...form.getInputProps('directions')} />
+        <TextInput mt="md" withAsterisk label="Recipe Name" placeholder="Apple Pie" {...form.getInputProps('name')} />
+        <Textarea mt="md" withAsterisk label="Ingredients (comma-separated)" placeholder="Apple, Flour, Cinnamon" {...form.getInputProps('ingredients')} />
+        <Textarea mt="md" withAsterisk label="Directions" placeholder="1. Preheat oven to 375 F..." {...form.getInputProps('directions')} />
 
         <Group justify="left" mt="md">
           <Button color="gray" onClick={() => form.reset()}>
